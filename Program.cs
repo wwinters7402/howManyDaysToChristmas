0bb9dc22df1a90a3d0b6eb0505e3bf2cd5633d7f
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-01-23 22:30:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LydiaDateSpan
+{
+
+    public class time
+
+    {
+
+
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            DateTime local = DateTime.Now;
+            var shortDateNow = DateTime.Now.ToShortDateString();
+            DateTime Christmas = new DateTime(2021, 12, 25);
+
+            TimeSpan daysToChristmas = Christmas - local;
+
+           
+
+           
+
+            Console.WriteLine("Lydia today is " + shortDateNow + ". And there are " + daysToChristmas.Days + " days until Christmas.");
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
